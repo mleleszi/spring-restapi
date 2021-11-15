c6041355cb18b5a4eac3f38d4bf75d801ddd0d60
mleleszi <mleleszi@icloud.com>
2021-11-15 15:14:54 +0100

    http GET, POST

--- src/main/java/hu/me/iit/webalk/airplane/spring_restapi/controller/AirplaneController.java
+++ src/main/java/hu/me/iit/webalk/airplane/spring_restapi/controller/AirplaneController.java
@@ -1,10 +1,37 @@
 package hu.me.iit.webalk.airplane.spring_restapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hu.me.iit.webalk.airplane.spring_restapi.service.AirplaneService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/airplane")
 public class AirplaneController {
+    private final AirplaneService airplaneService;
+
+    public AirplaneController(AirplaneService airplaneService) {
+        this.airplaneService = airplaneService;
+    }
+
+    @GetMapping
+    public Iterable<AirplaneDto> getAllAirplanes(){
+        List<AirplaneDto> airplaneDtoList = new ArrayList<>();
+        airplaneService.getAllAirplanes().forEach((a) -> airplaneDtoList.add(new AirplaneDto(a)));
+        return airplaneDtoList;
+    }
+
+    @GetMapping("/{id}")
+    public AirplaneDto getAirplaneById(@PathVariable Long id){
+        return new AirplaneDto(airplaneService.getById(id));
+    }
+
+    @PostMapping(consumes = "application/json")
+    public AirplaneDto save(@RequestBody @Valid AirplaneCreateDto airplaneCreateDto){
+        return new AirplaneDto(airplaneService.create(airplaneCreateDto.toAirplane()));
+    }
+
 
 }
