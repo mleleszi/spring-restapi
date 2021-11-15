75cb66ebc4594a4df91a71ed8458d3664d91cc75
mleleszi <mleleszi@icloud.com>
2021-11-15 13:28:38 +0100

    layer setup

---
+++ src/main/java/hu/me/iit/webalk/airplane/spring_restapi/controller/AirplaneController.java
@@ -0,0 +1,10 @@
+package hu.me.iit.webalk.airplane.spring_restapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/airplane")
+public class AirplaneController {
+
+}
---
+++ src/main/java/hu/me/iit/webalk/airplane/spring_restapi/service/AirplaneService.java
@@ -0,0 +1,8 @@
+package hu.me.iit.webalk.airplane.spring_restapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AirplaneService {
+
+}
